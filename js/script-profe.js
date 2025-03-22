f3f1b996d3d7822b830c9eb1ec7241a36c083362
gurutze-fagoaga <gurutzefagoaga@alumnos.cei.es>
2025-03-22 19:32:35 +0100

    player terminado

--- js/script-profe.js
+++ js/script-profe.js
@@ -1,6 +1,8 @@
 const listaBloques = document.querySelectorAll(".tab-content");
 const listaButtons = document.querySelectorAll(".nav-button");
-const containerCanciones = document.getElementById("containerCanciones");
+const containerCanciones = document.querySelector("#containerCanciones");
+const cancionActual = document.querySelector("#cancionActual");
+
 
 const listaTemas = [
     { id: 1, titulo: "Amaierak", artista: "Malko", imagen: "img/malko.jpeg", fuente: "./audio/amaierak.mp3" },
@@ -10,6 +12,7 @@ const listaTemas = [
     { id: 5, titulo: "Bueltatzen", artista: "Berri Txarrak", imagen: "img/berri-txarrak.jpg", fuente: "./audio/bueltatzen.mp3" }
 ];
 
+let idCancion = 0;
 let posicionTemaActual = 0;
 
 // -------------- NAVEGACIÓN --------------------//
@@ -55,9 +58,9 @@ listaTemas.forEach(tema => {
     //                                 </li>
     // `;
 
-    // agarrar el último hijo agregado para incluirle un eventListener 
-    // const cardCancion = containerCanciones.lastChild;
-    
+    // // agarrar el último hijo agregado para incluirle un eventListener 
+    // const cardCancion = containerCanciones.lastElementChild;
+
     const cardCancion = document.createElement('li');
     cardCancion.dataset.id = id;
     cardCancion.className = "cardCancion";
@@ -74,9 +77,11 @@ listaTemas.forEach(tema => {
         cargarCancion(id);
 
         // opcional : pasar a la pestaña de reproducción
-        document.querySelector("[data-tab]='tab3'").click();
+        //  document.querySelector("[data-tab]='tab3'").click();
 
     });
+
+    containerCanciones.appendChild(cardCancion);
 
 });
 
@@ -87,10 +92,10 @@ const prevBtn = document.querySelector(".fa-step-backward");
 const playBtn = document.querySelector(".fa-play");
 const pauseBtn = document.querySelector(".fa-pause");
 const nextBtn = document.querySelector(".fa-step-forward");
-const audioPlayer = document.querySelector("audioPlayer"); // nuestro reproductor
-const divCurrentSongInfo = document.querySelector("#paginaCancionActual");
+const audioPlayer = document.querySelector("#audioPlayer"); // nuestro reproductor
+const divCurrentSongInfo = document.querySelector(".divCurrentSongInfo");
 
-pauseBtn.style.display="none";
+pauseBtn.style.display = "none";
 
 
 // función para cargar un tema
@@ -99,47 +104,76 @@ function cargarCancion(id) {
     // recorremos nuestras canciones y obtenemos solo la que me interesa
     const cancion = listaTemas.find(tema => tema.id == id);
 
+    // si no encuentras ninguna canción, termina la función.
     if (!cancion) { return; };
+
+    const {titulo, imagen, artista, fuente} = cancion;
 
     console.log(cancion);
     audioPlayer.src = cancion.fuente;
-    idCancionActual = id;
+    idCancion = id;
 
-    paginaCancionActual.innerHTML = containerCanciones.innerHTML;
+    divCurrentSongInfo.innerHTML = `
+    
+                            <img src="./${imagen}" alt="Portada de canción" >
+                            <h2>${titulo}</h2>
+                            <small>${artista}</small>
+                            
+    
+    `
 
+    cancionActual.innerHTML = `
+    
+                            <img src="./${imagen}" alt="Portada de canción" >
+                            <h2>${titulo}</h2>
+                            <small>${artista}</small>
+                            
+    
+    `
 
     audioPlayer.play();
-    playBtn.style.display="none";
-    pauseBtn.style.display="inline";
+    playBtn.style.display = "none";
+    pauseBtn.style.display = "inline";
 
 };
 
 // Event Listeners de botones del reproductor
 
-playBtn.addEventListener("click", ()=> {
+playBtn.addEventListener("click", () => {
 
     audioPlayer.play();
-    playBtn.style.display="none";
-    pauseBtn.style.display="inline";
+    playBtn.style.display = "none";
+    pauseBtn.style.display = "inline";
 
 });
 
-pauseBtn.addEventListener("click", ()=> {
+pauseBtn.addEventListener("click", () => {
 
     audioPlayer.pause();
-    playBtn.style.display="inline";
-    pauseBtn.style.display="none";
+    playBtn.style.display = "inline";
+    pauseBtn.style.display = "none";
 
 });
 
 nextBtn.addEventListener("click", () => {
     posicionTemaActual++;
-    listaTemas[posicionTemaActual].id;
+    reiniciarReproduccion();
+    const idCancion = listaTemas[posicionTemaActual].id;
     cargarCancion(idCancion);
 });
 
 prevBtn.addEventListener("click", () => {
     posicionTemaActual--;
-    listaTemas[posicionTemaActual].id;
+    reiniciarReproduccion();
+    const idCancion = listaTemas[posicionTemaActual].id;
     cargarCancion(idCancion);
 });
+
+function reiniciarReproduccion() {
+    if (posicionTemaActual >= 5) {
+        posicionTemaActual = 0;
+    } else if (posicionTemaActual <= -1) {
+        posicionTemaActual = 4;
+    };
+    console.log(posicionTemaActual);
+};
--- js/script.js
+++ js/script.js
@@ -1,32 +1,40 @@
-// PARTE 1
+/*--------------- PARTE 1 -------------*/
 
+// NAVEGACIÓN
 const navegador = document.querySelector(".bottom-nav");
 const listaBotones = navegador.querySelectorAll(".nav-button");
 const listaContents = document.querySelectorAll(".tab-content");
+
+// LISTA
 const containerCanciones = document.getElementById("containerCanciones");
 const paginaCanciones = document.querySelector("#tab2");
 const cancionActual = document.querySelector("#cancionActual");
-const paginaCancionActual = document.querySelector("#paginaCancionActual");
 const container = document.querySelector(".container");
+
+const listaCanciones = [
+    { id: 28, titulo: "Amaierak", artista: "Malko", imagen: "img/malko.jpeg", fuente: "./audio/amaierak.mp3" },
+    { id: 2, titulo: "M.A.P.S", artista: "Amaia", imagen: "img/amaia.webp", fuente: "./audio/maps.mp3" },
+    { id: 35, titulo: "Fusilaren hotsa", artista: "La txama", imagen: "img/la-txama.jpg", fuente: "./audio/fusilaren-hotsa.mp3" },
+    { id: 4, titulo: "Ravean xuabe", artista: "Olaia Inziarte", imagen: "img/olaia-inziarte-zerrautsa.jpg", fuente: "./audio/ravean-xuabe.mp3" },
+    { id: 5, titulo: "Bueltatzen", artista: "Berri Txarrak", imagen: "img/berri-txarrak.jpg", fuente: "./audio/bueltatzen.mp3" }
+];
+
+// REPRODUCTOR
+const paginaCancionActual = document.querySelector("#paginaCancionActual");
 const btnAnterior = document.querySelector("#btnAnterior");
 const btnPlay = document.querySelector("#btnPlay");
 const btnPausa = document.querySelector("#btnPausa");
 const btnSiguiente = document.querySelector("#btnSiguiente");
-const audioPlayer = document.querySelector("audioPlayer"); // nuestro reproductor
-
-
-const listaCanciones = [
-    { id: 28, titulo: "Amaierak", artista: "Malko", imagen: "img/malko.jpeg", fuente: "audio/amaierak.mp3" },
-    { id: 1, titulo: "MAPS", artista: "Amaia", imagen: "img/amaia.webp", fuente: "audio/maps.mp3" },
-    { id: 2, titulo: "Fusilaren hotsa", artista: "La txama", imagen: "img/la-txama.jpg", fuente: "audio/fusilaren-hotsa.mp3" },
-    { id: 3, titulo: "Ravean xuabe", artista: "Olaia Inziarte", imagen: "img/olaia-inziarte-zerrautsa.jpg", fuente: "audio/ravean-xuabe.mp3" },
-    { id: 4, titulo: "Bueltatzen", artista: "Berri Txarrak", imagen: "img/berri-txarrak.jpg", fuente: "audio/bueltatzen.mp3" }
-];
+const audioPlayer = document.querySelector("#audioPlayer"); // nuestro reproductor
 
 let idCancion = 0;
+let posicionTemaActual = 0;
 
 
-// PARTE 2
+
+/*--------------- PARTE 2 -------------*/
+
+// NAVEGACIÓN
 
 listaBotones.forEach((btn, idx) => {
     btn.addEventListener("click", () => {
@@ -39,89 +47,106 @@ listaBotones.forEach((btn, idx) => {
     });
 });
 
+// LISTA
+
 listaCanciones.forEach(cancion => {
     // deconstrucción del objeto "tema"
     const { id, titulo, artista, imagen, fuente } = cancion;
 
-    // usamos innerHTML para crear nuestros temas
-    containerCanciones.innerHTML += `
-                                        <li id="${id}" class="cardCancion">
-                                            <img id="${id}" src="./${imagen}" alt="Artista 1">
-                                            <div id="${id}" >
-                                                <h2 id="${id}" >${titulo}</h2>
-                                                <small id="${id}" >${artista}</small>
-                                            </div>
-                                        </li>
+    const cardCancion = document.createElement('li');
+    cardCancion.dataset.id = id;
+    cardCancion.className = "cardCancion";
+    cardCancion.innerHTML = `
+                        <img src="./${imagen}" alt="Artista 1">
+                        <div>
+                            <h2>${titulo}</h2>
+                            <small>${artista}</small>
+                        </div>
+
 `
+    cardCancion.addEventListener("click", (event) => {
+        console.log(event.target.id);
+        // el id de los elementos que quiero que se reproduzcan al clickear = "id" de listaCanciones = la posición en el array
+        cargarCancion(id);
+    });
+
+    containerCanciones.appendChild(cardCancion);
 
 });
 
-// creamos la función de cargar la canción actual
+// REPRODUCTOR
 
-const handleCargarCancion = (id) => {
-    const titulo = listaCanciones[idCancion].titulo;
-    const artista = listaCanciones[idCancion].artista;
-    const imagen = listaCanciones[idCancion].imagen;
+btnPausa.style.display = "none";
+
+
+function cargarCancion(id) {
+
+    const tema = listaCanciones.find(cancion => cancion.id == id);
+
+    if (!tema) { return; };
+
+    const { titulo, imagen, artista } = tema;
+
+    audioPlayer.src = tema.fuente;
+    idCancion = id;
 
     cancionActual.innerHTML =
-    `
-    <h2>Reproducciendo...</h2>
+        `
+   
     <div class= "reproductor" >
         <img src = "${imagen}">
         <div class="reproductorInfo">
-            <h2 class="artista">${artista}</h2>
-            <span class="mp3">${titulo} </span>
+            <h2 class="artista">${titulo}</h2>
+            <span class="mp3">${artista} </span>
         </div>
     </div>
     `;
 
     paginaCancionActual.innerHTML = cancionActual.innerHTML;
 
-
-
-
+    audioPlayer.play();
+    btnPlay.style.display = "none";
+    btnPausa.style.display = "inline";
 };
 
-// funciones de los controllers
+// función para pasar de canción en bucle
+
+function reiniciarReproduccion() {
+    if (posicionTemaActual >= 5) {
+        posicionTemaActual = 0;
+    } else if (posicionTemaActual <= -1) {
+        posicionTemaActual = 4;
+    };
+    console.log(posicionTemaActual);
+};
+
+// controladores
+
+btnPlay.addEventListener("click", () => {
+    audioPlayer.play();
+    btnPlay.style.display = "none";
+    btnPausa.style.display = "inline";
+
+});
+
+btnPausa.addEventListener("click", () => {
+    audioPlayer.pause();
+    btnPlay.style.display = "inline";
+    btnPausa.style.display = "none";
+
+});
 
 btnSiguiente.addEventListener("click", () => {
-    idCancion++;
-    console.log(idCancion);
+    posicionTemaActual++;
     reiniciarReproduccion();
-    handleCargarCancion(idCancion);
+    const idCancion = listaCanciones[posicionTemaActual].id;
+    cargarCancion(idCancion);
 });
 
 btnAnterior.addEventListener("click", () => {
-    idCancion--;
+    posicionTemaActual--;
     reiniciarReproduccion();
-    handleCargarCancion(idCancion);
+    const idCancion = listaCanciones[posicionTemaActual].id;
+    cargarCancion(idCancion);
 });
 
-
-function reiniciarReproduccion() {
-    if (idCancion >= 5) {
-        idCancion = 0;
-    } else if (idCancion <= -1) {
-        idCancion = 4;
-    };
-    console.log(idCancion);
-};
-
-// llamamos a la canción actual al hacer click en las canciones
-
-paginaCanciones.addEventListener("click", (event) => {
-
-    console.log(event.target.id);
-    // el id de los elementos que quiero que se reproduzcan al clickear = "id" de listaCanciones = la posición en el array
-    idCancion = event.target.id;
-    handleCargarCancion(idCancion);
-
-
-});
-
-
-
-
-// PARTE 3: Ejecutamos la función de cargar la canción actual, con la canción nº 1 (idCancion=0)por defecto
-
-handleCargarCancion(idCancion);
